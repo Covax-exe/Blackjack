4dd697bcd949bef802aa3f0c506d439d139b4dd4
elwisstre <stivenvillada804@gmail.com>
2025-07-22 21:22:11 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,15 +1,1 @@
-@echo off
 
-echo Compilando Blackjack...
-g++ main.cpp Baraja1.cpp Carta1.cpp Croupier1.cpp Gestionador1.cpp Jugador1.cpp Mano1.cpp Participante1.cpp -o blackjack.exe
-
-if exist blackjack.exe (
-    echo ✅ Compilación exitosa.
-    echo Ejecutando...
-    blackjack.exe
-)
-else (
-    echo ❌ Error al compilar.
-)
-
-pause
